21499346704887da0d153691d1e3d77ed1339f9c
KalebMolewe23 <molewekaleb@gmail.com>
2022-07-23 20:03:18 +0700

    update logo title

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="<?= base_url("assets/logo/icon.png"); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewsport" content="width=device-width, initial-scale=1.0">
     <title>Administrator</title>
