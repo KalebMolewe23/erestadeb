975e7cd7852346f64cf82f02f235b7053f0d076a
KalebMolewe23 <molewekaleb@gmail.com>
2022-08-21 22:05:17 +0700

    update css admin

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <link rel="icon" href="<?= base_url("assets/logo/icon.png"); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewsport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
     <title><?= $title; ?></title>
     <!-- MATERIAL CDN -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
